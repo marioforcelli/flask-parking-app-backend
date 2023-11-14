bfc87ea82f69ae8993f2fa3ffaef5b89062838ab
Mario Melo <mario.melo@cmsw.com>
2023-11-14 16:10:36 -0300

    Inserted error handling at vehicles routes.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from app.models import Vehicle as v
-from app.db_conn import app
+from app.db_conn import app, db
 from app.services.vehicles import VehicleServices
 from flask import Flask, request, jsonify, Response
+from sqlalchemy.exc import IntegrityError
 
 @app.route("/")
 def hello():
@@ -10,50 +10,83 @@ def hello():
 
 @app.route("/vehicles/create", methods=['post'])
 def create():
-    req_data = request.get_json()
-    data = {
+    try:
+        req_data = request.get_json()
+        data = {
         "brand": req_data[0]["brand"],
         "color": req_data[0]["color"],
         "vehicle_type": req_data[0]["vehicle_type"],
         "license_plate": req_data[0]["license_plate"]
     }
-    VehicleServices.create_vehicle(vehicle_data=data)
-    return jsonify(data)
+        VehicleServices.create_vehicle(vehicle_data=data)
+        return jsonify(data)
+    
+    except IntegrityError as e:
+        db.session.rollback()
+        return {'message': 'Carro já existente na base de dados.'}, 400
+    
+    except KeyError as e:
+        db.session.rollback()
+        return {'message': f'O campo {str(e)} é obrigatório'}, 400
+    
+    except Exception:
+        return {'message': 'Erro inesperado'}, 500
+    
 
 @app.route("/vehicles/retrieve/<id>")
 def get(id):
-    vehicle = VehicleServices.get_vehicle(id)
-    return jsonify(vehicle), 200
 
+    try:
+        vehicle = VehicleServices.get_vehicle(id)
+        if not vehicle:
+            db.session.rollback()
+            return {'message' : 'Veículo não encontrado'}, 404
+        return jsonify(vehicle), 200
+    except Exception:
+        db.session.rollback()
+        return {'message', 'Erro inesperado'}, 500
 
 @app.route("/vehicles/list")
 def list():
-    return jsonify(VehicleServices.list_all_vehicles())
+    try:
+        return jsonify(VehicleServices.list_all_vehicles())
+    except Exception:
+        db.session.rollback()
+        return {'message', 'Erro inesperado'}, 500
+    
 
 @app.route("/vehicles/delete/<id>", methods=['DELETE'])
 def delete(id):
-    VehicleServices.delete_vehicle(id)
-    res = jsonify({
-        'message': 'Veículo deletado'
-    })
-    return res, 200
+    try:
+         VehicleServices.delete_vehicle(id)
+    except Exception:
+        db.session.rollback()
+        return {'message', 'Erro inesperado'}, 500
 
+
+   
 
 @app.route("/vehicles/update/<id>", methods=['POST'])
 def update(id):
-    vehicle = VehicleServices.get_vehicle(id)
 
-    if not vehicle:
-        return {'message': 'Veículo não encontrado'}, 404
-    
+    try:
+        vehicle = VehicleServices.get_vehicle(id)
 
-    req_data = request.get_json()
-    data = {
-        "brand": req_data[0]["brand"],
-        "color": req_data[0]["color"],
-        "vehicle_type": req_data[0]["vehicle_type"],
-        "license_plate": req_data[0]["license_plate"]
-    }
+        if not vehicle:
+            db.session.rollback()
+            return {'message': 'Veículo não encontrado'}, 404
+        
+
+        req_data = request.get_json()
+        data = {
+            "brand": req_data[0]["brand"],
+            "color": req_data[0]["color"],
+            "vehicle_type": req_data[0]["vehicle_type"],
+            "license_plate": req_data[0]["license_plate"]
+        }
+        
+        new_vehicle = VehicleServices.update_vehicle(id, data)
+        return jsonify(new_vehicle)
     
-    new_vehicle = VehicleServices.update_vehicle(id, data)
-    return jsonify(new_vehicle)
+    except Exception:
+        return {'message', 'Erro inesperado'}, 500
