ef4dde1f337ef9303c7da42db34d01c4c1bc3f80
Mario Melo <mario.melo@cmsw.com>
2023-11-14 10:31:43 -0300

    Concluded the first version for VehiclesService

--- app/services/vehicles.py
+++ app/services/vehicles.py
@@ -10,10 +10,32 @@ class VehicleServices:
         all_vehicles = Vehicle.query.all()
         return vehicle_schema.dump(all_vehicles)
     
-    def create(vehicle_data):
+    def create_vehicle(vehicle_data):
         new_vehicle = Vehicle(brand=vehicle_data["brand"], 
                               color=vehicle_data["color"], 
                               license_plate=vehicle_data["license_plate"],
                               vehicle_type=vehicle_data["vehicle_type"] )
         db.session.add(new_vehicle)
         db.session.commit()
+
+    def delete_vehicle(id_vehicle):
+        vehicle_schema = VehicleSchema()
+        vehicle = Vehicle.query.get(id_vehicle)
+        db.session.delete(vehicle)
+        db.session.commit()
+        return vehicle_schema.dump(vehicle)
+    
+    def get_vehicle(id_vehicle):
+        vehicle_schema = VehicleSchema()
+        vehicle = Vehicle.query.get(id_vehicle)
+        return vehicle_schema.dump(vehicle)
+    
+    def update_vehicle(id_vehicle, vehicle_data):
+        vehicle_schema = VehicleSchema()
+        vehicle = Vehicle.query.get(id_vehicle)
+        vehicle.brand = vehicle_data["brand"]
+        vehicle.license_plate = vehicle_data["license_plate"]
+        vehicle.vehicle_type=vehicle_data["vehicle_type"]
+        vehicle.color=vehicle_data["color"]
+        db.session.commit()
+        return vehicle_schema.dump(vehicle)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app.models import Vehicle as v
 from app.db_conn import app
 from app.services.vehicles import VehicleServices
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 
 @app.route("/")
 def hello():
@@ -17,11 +17,43 @@ def create():
         "vehicle_type": req_data[0]["vehicle_type"],
         "license_plate": req_data[0]["license_plate"]
     }
-    VehicleServices.create(vehicle_data=data)
+    VehicleServices.create_vehicle(vehicle_data=data)
     return jsonify(data)
+
+@app.route("/vehicles/retrieve/<id>")
+def get(id):
+    vehicle = VehicleServices.get_vehicle(id)
+    return jsonify(vehicle), 200
+
 
 @app.route("/vehicles/list")
 def list():
     return jsonify(VehicleServices.list_all_vehicles())
 
+@app.route("/vehicles/delete/<id>", methods=['DELETE'])
+def delete(id):
+    VehicleServices.delete_vehicle(id)
+    res = jsonify({
+        'message': 'Veículo deletado'
+    })
+    return res, 200
 
+
+@app.route("/vehicles/update/<id>", methods=['POST'])
+def update(id):
+    vehicle = VehicleServices.get_vehicle(id)
+
+    if not vehicle:
+        return {'message': 'Veículo não encontrado'}, 404
+    
+
+    req_data = request.get_json()
+    data = {
+        "brand": req_data[0]["brand"],
+        "color": req_data[0]["color"],
+        "vehicle_type": req_data[0]["vehicle_type"],
+        "license_plate": req_data[0]["license_plate"]
+    }
+    
+    new_vehicle = VehicleServices.update_vehicle(id, data)
+    return jsonify(new_vehicle)
